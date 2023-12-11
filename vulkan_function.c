9e350dfd234f0927e0f9b5ca7d58a591ef4e09b2
Nur Kholid <cocokcocok33@gmail.com>
2023-12-11 21:03:03 +0700

    header vulkan function

--- vulkan_function.c
+++ vulkan_function.c
@@ -1,4 +1,7 @@
 #include "vulkan_function.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 VkInstance createInstance() {
   VkApplicationInfo appInfo = {
@@ -10,9 +13,12 @@ VkInstance createInstance() {
       VK_MAKE_VERSION(1, 0, 0),
       VK_API_VERSION_1_0,
   };
+
   uint32_t glfwExtensionsCount = 0;
+
   const char **glfwExtensions =
       glfwGetRequiredInstanceExtensions(&glfwExtensionsCount);
+
   VkInstanceCreateInfo createInfo = {
       VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
       VK_NULL_HANDLE,
@@ -23,8 +29,29 @@ VkInstance createInstance() {
       glfwExtensionsCount,
       glfwExtensions,
   };
+
   VkInstance instance;
-  vkCreateInstance(&createInfo, VK_NULL_HANDLE, &instance);
+
+  const char **requiredExtension =
+      malloc((glfwExtensionsCount + 1) * sizeof(char *));
+
+  for (uint32_t i = 0; i < glfwExtensionsCount; i++) {
+    requiredExtension[i] = glfwExtensions[i];
+  }
+
+  requiredExtension[glfwExtensionsCount] =
+      VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME;
+
+  createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+  createInfo.enabledExtensionCount = glfwExtensionsCount + 1;
+  createInfo.ppEnabledLayerNames = requiredExtension;
+
+  if (vkCreateInstance(&createInfo, VK_NULL_HANDLE, &instance) != VK_SUCCESS) {
+    fprintf(stderr, "failed to create instance\n");
+    exit(EXIT_FAILURE);
+  }
+
+  free(requiredExtension);
 
   return instance;
 }
